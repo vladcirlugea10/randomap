2d4f97bf16d3349c1ecc7ae3f59110dc97e576bc
Nickolas Filip <nickolas.filip@opswat.com>
2025-08-21 14:12:14 +0300

    Chnaged region

--- main.tf
+++ main.tf
@@ -15,7 +15,33 @@ variable "student_identifier" {
 }
 
 provider "aws" {
-  region = "eu-central-1"
+  region = "us-east-1"
+}
+
+# This creates the "hat" (IAM Role) that App Runner can wear.
+# It has a trust policy that says "Only the App Runner service can wear me".
+resource "aws_iam_role" "app_runner_ecr_role" {
+  name = "AppRunnerECRAccessRole-${var.student_identifier}"
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "build.apprunner.amazonaws.com"
+        }
+        Action    = "sts:AssumeRole"
+      },
+    ]
+  })
+}
+
+# This attaches the "keys" (permissions) to the hat.
+# This specific AWS-managed policy gives all the permissions needed to pull from ECR.
+resource "aws_iam_role_policy_attachment" "app_runner_ecr_policy_attachment" {
+  role       = aws_iam_role.app_runner_ecr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
 }
 
 # This resource creates a private container registry in AWS to store our Docker images.
@@ -36,16 +62,25 @@ resource "aws_apprunner_service" "app_service" {
   service_name = "randomap-service-${var.student_identifier}"
 
   source_configuration {
-    # This magic line tells App Runner to automatically deploy when we push a new image.
+    # ADD THIS BLOCK
+    authentication_configuration {
+      access_role_arn = aws_iam_role.app_runner_ecr_role.arn
+    }
+
     auto_deployments_enabled = true
     image_repository {
       image_identifier      = "${aws_ecr_repository.app_repo.repository_url}:latest"
       image_repository_type = "ECR"
+
+      image_configuration {
+        port = "5000"
+      }
     }
   }
 
   instance_configuration {
-    port   = "5000"
+    cpu    = "256"
+    memory = "512"
   }
 }
 
